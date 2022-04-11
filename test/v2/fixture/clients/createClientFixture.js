92d21c5630f4cc0f3e03669e2d225288e5d3fed6
Esteban Córdoba <esteban.cordoba@moove-it.com>
2022-04-11 09:06:34 -0500

    change id in the url

--- test/v2/fixture/clients/createClientFixture.js
+++ test/v2/fixture/clients/createClientFixture.js
@@ -3,7 +3,7 @@ const createClientFixture = {
     id: 1234,
     name: 'Test',
     archive: false,
-    url: 'https://secure.tickspot.com/114217/api/v2/clients/430754.json',
+    url: 'https://secure.tickspot.com/114217/api/v2/clients/1234.json',
     updated_at: '2022-04-10T11:59:52.000-04:00',
   },
 };
